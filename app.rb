8511495200b5039e3eff87181befbf2e0187f532
Andrew Nesbitt <andrewnez@gmail.com>
2017-04-14 10:37:27 +0100

    Handle invalid semver versions better

--- app.rb
+++ app.rb
@@ -46,6 +46,8 @@ More info: https://libraries.io/#{platform.downcase}/#{name}/#{version}"
     requiremnts.none? do |requirement|
       SemanticRange.gtr(version, requirement, false, platform)
     end
+  rescue
+    false
   end
 
   def prerelease?(platform, version)
